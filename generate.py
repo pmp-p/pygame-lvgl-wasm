6449ed4afec2294af725ba442e85adf4d5a32f65
ckielstra <c.kielstra@ultimaker.com>
2021-11-16 15:48:58 +0100

    E1-898 Removed MicroPython support

--- generate.py
+++ generate.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python3
 import sourceparser
-from micropython import MicroPythonBindingsGenerator
 from python import PythonBindingsGenerator
 
 parseresult = sourceparser.LvglSourceParser().parse_sources('lvgl')
-#mpy_gen = MicroPythonBindingsGenerator(parseresult)
-#mpy_gen.generate()
 py_gen = PythonBindingsGenerator(parseresult)
 py_gen.generate()
-
