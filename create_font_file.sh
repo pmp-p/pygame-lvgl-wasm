0a090095f0505a27e19493d902bc12c5d3c0934d
ckielstra <c.kielstra@ultimaker.com>
2022-03-17 19:26:11 +0100

    Remove two large glyphs from the font file for reduced line height

--- create_font_file.sh
+++ create_font_file.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 #
-# This script will convert a font file to a bit-map oriented format usable by LVGL.
+# This script will convert a high-resolution font file to a bit-map oriented format usable by LVGL.
 # For details: https://github.com/lvgl/lv_font_conv
 #
 # Preconditions
-# Install the font conversion tool: 
+# Install the LVGL font conversion tool:
 #    npm i lv_font_conv -g
 
 # The NotoSans font file was downloaded from https://github.com/googlefonts/noto-cjk/tree/main/Sans/OTF/SimplifiedChinese
-lv_font_conv --bpp 2 --size 18 --font NotoSansCJKsc-Regular.otf -r 0x20-0xFFFF --format lvgl --force-fast-kern-format -o noto_sans_cjk_18_2bpp.c
+# Convert the whole visible Unicode range, i.e. character code positions 0x20 to 0xFFFF
+# There are 2 glyphs with huge impact on the total line height, resulting in a total box height of 34 where we target around 20.
+# We adopted the range to exclude U+3031 and U+3032, these are two kanji repeat characters that we can do without.
+lv_font_conv --bpp 2 --size 18 --font NotoSansCJKsc-Regular.otf -r 0x20-0x3030,0x3033-0xFFFF --format lvgl --force-fast-kern-format -o noto_sans_cjk_18_2bpp.c
